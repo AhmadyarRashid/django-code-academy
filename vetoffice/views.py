d40c96e75c0a22716ec8218e5464e17602727b25
AhmadyarRashid <ahmadyarmeo@gmail.com>
2023-02-27 20:50:10 +0500

    Class Based Views

--- vetoffice/views.py
+++ vetoffice/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Owner, Patient
+from django.views.generic import ListView
 
 pets = [
   { "petname": "Fido", "animal_type": "dog"},
@@ -6,6 +8,14 @@ pets = [
   { "petname": "Cleo", "animal_type": "cat"},
   { "petname": "Oreo", "animal_type": "dog"},
 ]
+
+class OwnerList(ListView):
+    model = Owner
+    template_name = "vetoffice/owner_list.html"
+
+class PatientList(ListView):
+    model = Patient
+    template_name = "vetoffice/patient_list.html"
 
 # Create your views here.
 def home(request):
