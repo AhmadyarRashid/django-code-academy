c33cd05e64572e47fc0aa4a9d34457fe28c64c2c
AhmadyarRashid <ahmadyarmeo@gmail.com>
2023-02-27 21:18:17 +0500

    implement all CRUD views

--- vetoffice/views.py
+++ vetoffice/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Owner, Patient
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 pets = [
   { "petname": "Fido", "animal_type": "dog"},
@@ -9,15 +10,43 @@ pets = [
   { "petname": "Oreo", "animal_type": "dog"},
 ]
 
+# Create your views here.
+def home(request):
+    context = {"name": "Djangoer", "pets": pets}
+    return render(request, 'vetoffice/home.html', context)
+
+class OwnerCreate(CreateView):
+    model = Owner
+    template_name = "vetoffice/owner_create_form.html"
+    fields = ["first_name", "last_name", "phone"]
+
+class OwnerUpdate(UpdateView):
+    model = Owner
+    template_name = "vetoffice/owner_update_form.html"
+    fields = ["first_name", "last_name", "phone"]
+
 class OwnerList(ListView):
     model = Owner
     template_name = "vetoffice/owner_list.html"
+
+class OwnerDelete(DeleteView):
+    model = Owner
+    template_name = "vetoffice/owner_delete_form.html"
+
+class PatientCreate(CreateView):
+    model = Patient
+    template_name = "vetoffice/patient_create_form.html"
+    fields = ["animal_type", "breed", "pet_name", "age", "owner"]
+
+class PatientUpdate(UpdateView):
+    model = Patient
+    template_name = "vetoffice/patient_update_form.html"
+    fields = ["animal_type", "breed", "pet_name", "age", "owner"]
 
 class PatientList(ListView):
     model = Patient
     template_name = "vetoffice/patient_list.html"
 
-# Create your views here.
-def home(request):
-    context = {"name": "Djangoer", "pets": pets}
-    return render(request, 'vetoffice/home.html', context)
+class PatientDelete(DeleteView):
+    model = Patient
+    template_name = "vetoffice/patient_delete_form.html"
