63819f60d8f0bcb9890b36d7ca1bad709f32eafb
AhmadyarRashid <ahmadyarmeo@gmail.com>
2023-03-18 11:31:18 +0500

    create models view, forms and urls

--- tourist_attractions_form/urls.py
+++ tourist_attractions_form/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('details/<statename>', views.details, name="details")
+    path('details/<statename>', views.details, name="details"),
+    path('state/create', views.StateCreate.as_view(), name="statecreate"),
+    path('attraction/create', views.AttractionCreate.as_view(), name="attractioncreate")
 ]
--- tourist_attractions_form/views.py
+++ tourist_attractions_form/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import State, Attraction
+from .forms import StateCreateForm, AttractionCreateForm
 
 # Create your views here.
 def home(request):
@@ -11,3 +13,13 @@ def details(request, statename):
     attractions = Attraction.objects.all()
     context = { "attractions": attractions, "statename": statename.replace("-", "")}
     return render(request, "tourist_attractions/details.html", context)
+
+class StateCreate(CreateView):
+    model = State
+    form_class = StateCreateForm
+    template_name = ""
+
+class AttractionCreate(CreateView):
+    model = Attraction
+    form_class = AttractionCreateForm
+    template_name = ""
